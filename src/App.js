97070c3512bdd92c93a7a18ae26dddddf9a6b604
Emerson Espinoza <xinayespinoza@gmail.com>
2023-06-03 21:35:51 -0400

    estados y contador de todo

--- src/App.js
+++ src/App.js
@@ -8,16 +8,26 @@ import React from 'react';
 
 const defaultTodos = [
   { text: "Cortar pelo", completed: true },
-  { text: "Liceos en Antofa", completed: true },
+  { text: "Liceos en Antofagasta", completed: true },
   { text: "Llorar con la llorona", completed: false },
   { text: "Cortar panes", completed: false },
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos)
+  const [searchValue, setSearchValue] = React.useState('');
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  console.log('los usuario buscan todos de ' + searchValue);
+
   return (
     <>
-      <TodoCounter completed={16} total={30} />
-      <TodoSearch />
+      <TodoCounter completed={completedTodos} total={totalTodos} />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList  >
         {defaultTodos.map(todo => (
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,13 +1,19 @@
 import './TodoSearch.css'
-function TodoSearch() {
+import React from 'react'
+function TodoSearch({
+  searchValue,
+  setSearchValue,
+}) {
+
   return (
 
-    <input className="TodoSearch" placeholder="Buscar Todo "
+    <input
+      className="TodoSearch"
+      placeholder="Buscar Todo "
+      value={searchValue}
       onChange={(event) => {
-        console.log("Escribiste en el TodoSearch")
-        console.log(event)
-        console.log(event.target)
-        console.log(event.target.value)
+        setSearchValue(event.target.value)
+
       }}
     />
 
