f17d46e9615531eef4bd833f2adfdeac182b38de
Emerson Espinoza <xinayespinoza@gmail.com>
2023-06-03 22:19:52 -0400

    completando y eliminando todos

--- src/App.js
+++ src/App.js
@@ -18,6 +18,31 @@ function App() {
   const [searchValue, setSearchValue] = React.useState('');
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
+  const searchTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText)
+    }
+  );
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  }
+
 
   console.log('los usuario buscan todos de ' + searchValue);
 
@@ -30,11 +55,13 @@ function App() {
       />
 
       <TodoList  >
-        {defaultTodos.map(todo => (
+        {searchTodos.map(todo => (
           <TodoItem
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
 
           />
         ))}
