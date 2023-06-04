c4b14cfdb30ef8e36c5f70a971c96ad6f4c7bbc7
Emerson Espinoza <xinayespinoza@gmail.com>
2023-06-04 00:03:06 -0400

    fix completando

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,25 +1,20 @@
+import { CompleteIcon } from './CompleteIcon'
+import { DeleteIcon } from './DeleteIcon'
 import './TodoItem.css'
 function TodoItem(props) {
   return (
 
     <li className="TodoItem">
-
-      <span
-        className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
-        onClick={props.onComplete}
-      > v</span>
+      <CompleteIcon
+        completed={props.completed}
+        onCompleted={props.onCompleted}
+      />
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"} `}>{props.text}</p>
-      <span
-        className="Icon Icon-delete"
-        onClick={props.onDelete}
-      >X</span>
-
+      <DeleteIcon
+        onDelete={props.onDelete}
+      />
     </li>
-
-
-
   )
-
 }
 
 export { TodoItem }
